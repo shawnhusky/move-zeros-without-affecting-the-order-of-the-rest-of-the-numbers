cc1a91c174eeeaeb96188782c1e97c48fd19135b
Shawn <74233354+shawnhusky@users.noreply.github.com>
2021-07-24 07:09:56 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+array1 = [0,1,0,3,12]
+array2 = [1,7,0,0,8,0,10,12,0,4]
+
+def solution(nums):
+    for i in nums:
+        if 0 in nums:
+            nums.remove(0) #remove element
+            nums.append(0) #this will add at the end of the list
+    return nums
+
+
+print(solution(array1))
+print(solution(array2))
